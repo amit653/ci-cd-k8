ed5f834ab7850fab890eea1d45989b4be9a60cb3
amit653 <amit653@gmail.com>
2026-07-15 16:43:17 +0000

    manifest

--- my-app/app.py
+++ my-app/app.py
@@ -1,15 +1,17 @@
-import psycopg2, os
+import psycopg2, os,time
+from datetime import datetime
+from flask import Flask
 
+app=Flask(__name__)
 DB_CONFIG = {
     'dbname': 'student',
     'user': 'postgres',
     'password': 'postgres',
-    #'host': 'pg_test-postgres-1',
-    'host': 'x.x.xx.xx',
+    #'host': '192.x.x',
+    'host': 'postgres',  ## service name of postgres pod
     'port': 5432,
     'connect_timeout': 5  # Tell psycopg2 to wait up to 5s per attempt
 }
-
 def get_db_connection():
     """Attempts to connect to Postgres with a retry loop for startup."""
     while True:
@@ -20,9 +22,17 @@ def get_db_connection():
             print(f"[{datetime.now().strftime('%H:%M:%S')}] Postgres not ready yet. Retrying in 2s...")
             time.sleep(2)
 
-
-if __name__== '__main__':
- conn = get_db_connection()
+@app.route("/")
+def display():
+ conn=get_db_connection()
  cur = conn.cursor()
  cur.execute("SELECT version();")
- print("Postgres version:", cur.fetchone())
+ version=cur.fetchone()
+ cur.close()
+ conn.close()
+ return f"Connected to Postgres! Version: {version}"
+
+if __name__== '__main__':
+ app.run(host="0.0.0.0",port=5000)
+ 
+ #print("Postgres version:", cur.fetchone())
